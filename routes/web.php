8d0f75fc492e9af70abc348102237ace7892638e
Riccardo Colombo <rikyregna@gmail.com>
2026-06-30 17:58:47 +0200

    Rimuove welcome.blade.php e route web: backend API-only

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Backend API-only: nessuna route web (niente Blade/sessioni).
+// Tutte le route applicative vivono in routes/api.php.
